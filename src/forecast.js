54782862f3f66a36afb34433de32d39949f71571
Michal Kolanecki <michalkolanecki@gmail.com>
2021-09-09 14:40:21 +0200

    Create function which get weather information

--- src/forecast.js
+++ src/forecast.js
@@ -1,5 +1,15 @@
 const key = 'LBzjvxs9k0p7AytB7gNItXTNyGuTuXt9';
 
+const getWeatherInfo = async (id) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
 const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
@@ -11,5 +21,10 @@ const getCity = async (city) => {
 };
 
 getCity('warszawa')
-    .then(data => console.log(data))
+    .then(data => {
+        return getWeatherInfo(data.Key)
+    })
+    .then(data => {
+        console.log(data);
+    })
     .catch(err => console.log(err));
